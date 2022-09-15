3c2c07d2f9914d61894f3b86b1eef05eafc307ae
Sploder12 <trevorschupbach@gmail.com>
2022-09-15 18:36:24 -0400

    boot loader :)

---
+++ src/boot/gdt.asm
@@ -0,0 +1,34 @@
+gdt_start:
+    ; the GDT starts with 8 bytes of 0
+    dd 0x0
+    dd 0x0
+
+; GDT for code segment. base = 0x00000000, length = 0xfffff
+; for flags, refer to os-dev.pdf document, page 36
+gdt_code: 
+    dw 0xffff    ; segment length, bits 0-15
+    dw 0x0       ; segment base, bits 0-15
+    db 0x0       ; segment base, bits 16-23
+    db 10011010b ; flags (8 bits)
+    db 11001111b ; flags (4 bits) + segment length, bits 16-19
+    db 0x0       ; segment base, bits 24-31
+
+; GDT for data segment. base and length identical to code segment
+; some flags changed
+gdt_data:
+    dw 0xffff
+    dw 0x0
+    db 0x0
+    db 10010010b
+    db 11001111b
+    db 0x0
+
+gdt_end:
+
+gdt_descriptor:
+    dw gdt_end - gdt_start - 1 ; size (16 bit)
+    dd gdt_start ; address (32 bit)
+
+; locations for gdt segments
+CODE_SEG equ gdt_code - gdt_start
+DATA_SEG equ gdt_data - gdt_start
