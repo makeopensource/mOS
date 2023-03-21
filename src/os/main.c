515e282d8a372b58779cc19c9a288d8c32d20ed3
Shawn z <zimmermannshawn@gmail.com>
2023-03-20 23:35:40 -0400

    Initialize and sleep test

--- src/os/main.c
+++ src/os/main.c
@@ -1,20 +1,25 @@
 #include "hard/idt.h"
 
 #include "video/VGA_text.h"
+#include "pit/pit.h"
 
 int os_main(){
     makeInterruptTable();
-
+		init_pit();
+		
     clearScreen(black);
 
     writeText("Welcome To mOS!", (80 - 15)/2, 5, red);
 
-    println("It booted!!!", green);
+		println("It booted!!!", green);
 
     VGA_Color colour = light_cyan;
     const char *string = "Hello, World!";
     println(string, colour);
-    
+
+		 println("Timer Start", red);
+		sleep(100);
+		println("Timer End", red);
 
     while (1==1)
         ;
