5d816460f37b021e58885553fe760c703757076b
Emil Kovacev <emilkova@buffalo.edu>
2023-03-26 22:50:00 -0400

    replaced floating point math with division

--- src/lib/stdlib/stdio.c
+++ src/lib/stdlib/stdio.c
@@ -15,7 +15,6 @@ int sn_printf( char *restrict buffer, size_t bufsz,
     unsigned char c;    // printf("%c\n", 'c');
     char *s;            // printf("%s\n", "hello, world!");
     int i;              // printf("%i\n", 42);
-    // double f;        // printf("%f\n", 42.0);
 
     va_start(ap, format);
     int n = 0;
@@ -29,56 +28,58 @@ int sn_printf( char *restrict buffer, size_t bufsz,
             char fmtchar = *p;
             size_t strlen;
             switch(fmtchar) {
-                // print "%"
-                case '%':
-                    *buffer = *p;
-                    buffer++;
+            
+            // print "%"
+            case '%':
+                *buffer = *p;
+                buffer++;
+                break;
 
-                // string formatting
-                case 's':
-                    s = va_arg(ap, char *);
-                    strlen = strnlen_s(s, MAX_SNPRINTF_STRING);
+            // string formatting
+            case 's':
+                s = va_arg(ap, char *);
+                strlen = strnlen_s(s, MAX_SNPRINTF_STRING);
 
-                    // checks if there is space for the entire string
-                    // plus the null-terminating byte
-                    if (strlen + n + 1 < bufsz) {
-                        memcpy(buffer, s, strlen);
-                        buffer += strlen;
-                    } else {
-                        return n;
-                    }
+                // checks if there is space for the entire string
+                // plus the null-terminating byte
+                if (strlen + n + 1 < bufsz) {
+                    memcpy(buffer, s, strlen);
+                    buffer += strlen;
+                } else {
+                    return n;
+                }
 
-                    break;
+                break;
 
-                // character formatting
-                case 'c':
-                    c = (char) va_arg(ap, int);
-                    memcpy(buffer, &c, sizeof(char));
-                    buffer++;
-                    break;
+            // character formatting
+            case 'c':
+                c = (char) va_arg(ap, int);
+                memcpy(buffer, &c, sizeof(char));
+                buffer++;
+                break;
+            
+            default:
+                *buffer = *p;
+                buffer++;
 
-                case 'i':
-                    i = va_arg(ap, int);
-                    int digits = 0;
-                    for (int temp = i; temp >= 1; temp *= 0.1, digits++)
-		                ;
-                    char temp_buffer[digits];
-                    itoa(i, temp_buffer);
-                    strlen = strnlen_s(temp_buffer, MAX_SNPRINTF_STRING);
+            case 'i':
+                i = va_arg(ap, int);
+                int digits = 0;
+                for (int temp = i; temp >= 1; temp = temp / 10, digits++)
+                            ;
+                char temp_buffer[digits];
+                itoa(i, temp_buffer);
+                strlen = strnlen_s(temp_buffer, MAX_SNPRINTF_STRING);
 
-                    // checks if there is space for the entire string
-                    // plus the null-terminating byte
-                    if (strlen + n + 1 < bufsz) {
-                        memcpy(buffer, temp_buffer, strlen);
-                        buffer += strlen;
-                    } else {
-                        return n;
-                    }
-                // case 'f':
-                //     f = va_arg(ap, double);
-                // default:
-                //     *buffer = *p;
-                //     buffer++;
+                // checks if there is space for the entire string
+                // plus the null-terminating byte
+                if (strlen + n + 1 < bufsz) {
+                    memcpy(buffer, temp_buffer, strlen);
+                    buffer += strlen;
+                } else {
+                    return n;
+                }
+                break;
             }
         }
     }
