b6618ee13b57aeaad686ebfdce31ae4f2b1e52a4
Real-Septicake <septicake056@gmail.com>
2024-10-30 19:34:40 -0400

    test_keyboard now uses new assert macro, chkPosCMD now takes line and column positions

--- tests/src/lib/stdlib/test_keyboard.c
+++ tests/src/lib/stdlib/test_keyboard.c
@@ -11,7 +11,7 @@ extern int highlight_offset;
 extern VGA_Char *cursor;
 
 // enum representing the kinds of commands available
-enum CMD { keyPress, checkOffset, checkPosition, end };
+enum CMD { keyPress, checkOffset, checkPosition, setPosition, end };
 
 struct TestCMD {
     enum CMD cmd;
@@ -36,8 +36,13 @@ struct TestCMD chkOffCMD(int expected) {
 
 // creates a position check command
 // expected position is relative to the beginning of vga memory
-struct TestCMD chkPosCMD(unsigned expected) {
-    return (struct TestCMD){checkPosition, {.offset = expected}};
+struct TestCMD chkPosCMD(unsigned line, unsigned col) {
+    return (struct TestCMD){checkPosition,
+                            {.offset = (line * VGA_WIDTH) + col}};
+}
+
+struct TestCMD setPosCMD(unsigned line, unsigned col) {
+    return (struct TestCMD){setPosition, {.offset = (line * VGA_WIDTH) + col}};
 }
 
 // creates an end command
@@ -50,12 +55,17 @@ void execCMD(struct TestCMD cmd) {
     case checkOffset: // asserts that the current highlight offset matches the
                       // supplied offset
         ASSERT_M(highlight_offset == cmd.data.offset,
-                 "Highlight difference | ");
+                 "Highlight difference | Expected: %i, Actual: %i",
+                 cmd.data.offset, highlight_offset);
         break;
     case checkPosition: // asserts that the relative position from the start of
                         // vga memory matches the supplied position
         ASSERT_M(cursor - VGA_MEMORY == cmd.data.offset,
-                 "Position difference | ");
+                 "Position difference | Expected: %i, Actual: %i",
+                 cmd.data.offset, cursor - VGA_MEMORY);
+        break;
+    case setPosition:
+        cursor = VGA_MEMORY + cmd.data.offset;
         break;
     case keyPress: // simulates a keypress
         vgaEditor(cmd.data.kb);
@@ -67,31 +77,35 @@ void execCMD(struct TestCMD cmd) {
 }
 
 void test_main() {
+    cursor = VGA_MEMORY + 160;
     struct TestCMD b[] = {
         // The position checks assume that only 2 lines of text have been
         // written during boot
-        chkPosCMD(160),
+        chkPosCMD(2, 0),
         kbCMD(Key_a, KeyPressed, 0),
         chkOffCMD(0),
-        chkPosCMD(161),
+        chkPosCMD(2, 1),
         kbCMD(Key_left, KeyPressed, KEY_MOD_SHIFT),
         chkOffCMD(-1),
-        chkPosCMD(160),
+        chkPosCMD(2, 0),
         kbCMD(Key_up, KeyPressed, KEY_MOD_SHIFT),
         chkOffCMD(-81),
-        chkPosCMD(80),
+        chkPosCMD(1, 0),
         kbCMD(Key_up, KeyPressed, KEY_MOD_SHIFT),
         chkOffCMD(-161),
-        chkPosCMD(0),
+        chkPosCMD(0, 0),
         kbCMD(Key_b, KeyPressed, 0),
         chkOffCMD(0),
-        chkPosCMD(1),
+        chkPosCMD(0, 1),
         kbCMD(Key_down, KeyPressed, KEY_MOD_SHIFT),
         chkOffCMD(80),
-        chkPosCMD(81),
+        chkPosCMD(1, 1),
         kbCMD(Key_up, KeyPressed, 0),
         chkOffCMD(0),
-        chkPosCMD(1),
+        chkPosCMD(0, 1),
+        setPosCMD(0, 79),
+        kbCMD(Key_right, KeyPressed, 0),
+        chkPosCMD(1, 0),
         // Remember to end command sequence!
         endCMD(),
     };
