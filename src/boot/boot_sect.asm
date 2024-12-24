ae7faeba307d708cd5c9c94513b7b9d318fee1f1
Alex <asbohosi@buffalo.edu>
2024-12-24 00:42:45 -0500

    Improved error handling of boot sector code and other changes.

--- src/boot/boot_sect.asm
+++ src/boot/boot_sect.asm
@@ -1,13 +1,10 @@
 [org 0x7c00]
 OS_OFFSET equ 0x1000
+MAX_READ_TRIES equ 3
+SECTORS_TO_READ equ 22
 
 [bits 16]
 begin:
-    jmp code
-    RETRY_COUNT dw 0
-    DRIVE_N_HEADS db 0
-    DRIVE_SECTS_PER_TRACK db 0
-code:
     mov [BOOT_DRIVE], dl
     mov ax, 0
     mov ds, ax
@@ -20,7 +17,21 @@ code:
 
 ;;; Get Drive Parameters
     mov ah, 8
+    mov dl, [BOOT_DRIVE]
+    mov di, 0
+    clc
     int 0x13
+    jnc get_params_no_error
+;;; Print and stop if there is an error reading the drive parameters
+    mov al, ah
+    call print_byte
+    call print_cm
+    mov al, 'p'
+    call print_char
+    jmp $
+
+get_params_no_error:
+;;; Save some of the drive parameters
     add dh, 1
     mov [DRIVE_N_HEADS], dh
     and cl, 0x3f
@@ -33,101 +44,80 @@ code:
 [bits 16]
 load_kernel:
 
-;;; Read Drive
+;;; Read drive one sector at a time
     mov bl, 1
 read:
-    mov al, bl ;sector to read
+    mov al, bl ; current sector to read
     call read_drive
     add bl, 1
-    cmp bl, 24
-    jne read
-
-    mov ax, [OS_OFFSET]
-    cmp ax, 0x03e8
-    ;; jne error
+    cmp bl, SECTORS_TO_READ
+    jle read
     call enter_pm
 
-error:
-        mov bx, OS_OFFSET
-        mov cx, bx
-        add cx, 0x200
-error_out:
-        mov ax, [bx]
-        call print_word
-        call print_nl
-        add bx, 4
-        cmp bx, cx
-        jne error_out
-        jmp $
-
-;;; Reads from drive in reg dl, read sector LBA al
+;;; Reads one sector from the drive at the given LBA into the kernel memory
+;;; space at the appropriate offset (assumming 512 byte sectors).
+;;; Input:
+;;;   [al] = LBA of sector to read
+;;; Output:
+;;;   [cf] = Set on error
+;;;   [ah] = status, as returned by INT 0x13/AH=0x02
+;;;   [al] = sectors transferred, as returned by INT 0x13/AH=0x02
 read_drive:
-    push bx
     push cx
-        
+    push bx
+    push ax
+
+    mov byte [READ_TRY_COUNT], 0
+read_drive_retry:
+    add byte [READ_TRY_COUNT], 1
+    mov cx, 0
+    mov es, cx ; Ensure segment register [es] is zero for interrupt
 ;;; Calculate Offset for Kernel
     mov cl, al
-    mov ch, 0
     sub cl, 1
     shl cx, 9
     mov bx, OS_OFFSET
     add bx, cx
-    push bx
 
 ;;; Calculate C,H,S using algorithm from OSDev:
 ;;; https://wiki.osdev.org/Disk_access_using_the_BIOS_(INT_13h)
-    mov ah, 0
+    mov ah, 0 ; Ensure top half of [ax] is zero so that [al] behaves as dividend
     div byte [DRIVE_SECTS_PER_TRACK]
     add ah, 1
-    mov bl, ah
+    mov cl, ah
     mov ah, 0
     div byte [DRIVE_N_HEADS]
     mov dh, ah
     mov ch, al
-    mov cl, bl
-
-    pop bx
-    push ax
-    mov ax, es
-    call print_word
-    call print_cm
-    mov ax, 0
-    mov es, ax
-    pop ax
     mov al, 1
+    mov ah, 2
     mov dl, [BOOT_DRIVE]
     clc
-    mov ah, 2
     int 0x13 ;do read
-    mov bx, 0
-    mov es, bx
-    call print_word
-    call print_nl
+    jnc read_drive_no_error
 
-    pop cx
-    pop bx
-    ret
-        
-retry_drive:
-;;; Print Sectors Transferred
-    push ax ; Save error code
-    add al, 48
-    call print_char
-    mov al, 44
-    call print_char
-    pop ax ; Retreive error code
+;;; If an error occurred during the read:
+    call print_byte
+    call print_cm
     mov al, ah
-    add al, 48
-    call print_char
-    mov al, 10
-    call print_char
-
-    mov ax, [RETRY_COUNT]
-    add ax, 1
-    mov [RETRY_COUNT], ax
-    cmp ax, 1000
-    jne load_kernel
+    call print_byte
+    call print_cm
+    mov bl, [READ_TRY_COUNT]
+    mov al, bl
+    call print_byte
+    call print_cm
+    mov ax, [esp] ; Restore [al] (LBA of sector to read from)
+    call print_byte
+    call print_nl
+    cmp bl, MAX_READ_TRIES
+    jl read_drive_retry
     jmp $
+
+read_drive_no_error:
+    pop bx ; Pop old value of ax
+    pop bx
+    pop cx
+    ret
 
 ;;; Prints character in register al
 print_char:
@@ -142,6 +132,7 @@ print_char:
     ret
 
 print_half_byte:
+    pushfd
     and al, 0x0F
     add al, 48
     cmp al, 57
@@ -149,6 +140,7 @@ print_half_byte:
     add al, 7
 skip_hex:
     call print_char
+    popfd
     ret
         
 print_byte:
@@ -175,9 +167,9 @@ print_word:
 
 print_nl:
     push ax
-    mov al, 32
+    mov al, 10
     call print_char
-    mov al, 32
+    mov al, 13
     call print_char
     pop ax
     ret
@@ -195,9 +187,11 @@ begin_pm:
     hlt
 
 
-times 509 - ($ - $$) db 0 ;padding
+times 506 - ($ - $$) db 0 ;padding
 
-
+READ_TRY_COUNT db 0
+DRIVE_N_HEADS db 0
+DRIVE_SECTS_PER_TRACK db 0
 BOOT_DRIVE db 0 ;0x7dfd
 ;above is data that can always be found at 0x7dfd - n during boot process
 
